0bbe356cfd3e8884029be67e705317af306a4717
RaghuprakashH <75564113+RaghuprakashH@users.noreply.github.com>
2021-09-13 18:32:45 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its Air Temp Q package",
+    author="Raghu",
+    packages=find_packages(),
+    license="MIT"
+)
